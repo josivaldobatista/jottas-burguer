b9a82330355f1dce23e5ea5d8dfc738f0e9417ef
Josivaldo Batista <josivaldo-fb@outlook.com>
2025-01-21 17:29:09 -0300

    first commit

---
+++ src/main/kotlin/com/jfb/jottasburguer/JottasBurguerApplication.kt
@@ -0,0 +1,11 @@
+package com.jfb.jottasburguer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JottasBurguerApplication
+
+fun main(args: Array<String>) {
+	runApplication<JottasBurguerApplication>(*args)
+}
---
+++ src/test/kotlin/com/jfb/jottasburguer/JottasBurguerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jfb.jottasburguer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JottasBurguerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
